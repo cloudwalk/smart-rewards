684d7b4b932efc731d70d108913334bdc4050669
moreira-a <arthur.moreira@cloudwalk.io>
2021-03-20 02:25:52 -0300

    continue testing BRLC token mock

--- test/SpinMachineV1.ts
+++ test/SpinMachineV1.ts
@@ -1,8 +1,8 @@
-//import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { ContractFactory, Contract } from "ethers";
+import { ContractFactory, Contract, VoidSigner, Wallet } from "ethers";
+import { smockit, smoddit } from "@eth-optimism/smock"
 
 describe("smart-rewards tests", async function() {
   let SpinMachineV1: ContractFactory;
@@ -10,12 +10,25 @@ describe("smart-rewards tests", async function() {
   let owner: SignerWithAddress;
   let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
+  let brlc_address = "0x6275c7A100A6d16035DEa9930E18890bE42185A7";
+  let provider = ethers.getDefaultProvider('rinkeby');
+  let signer = ethers.Wallet.createRandom().connect(provider);
+  let abi = [
+    "function safeTransferFrom(IERC20Upgradeable token, address from, address to, uint256 value) internal"
+  ]
+  let brlc_token = await new ethers.Contract(brlc_address, abi, signer);
+  let mocked_brlc = await smockit(brlc_token);
+  console.log(brlc_token.address)
 
   beforeEach(async function() {
     SpinMachineV1 = await ethers.getContractFactory("SpinMachineV1");
     spinMachine = await upgrades.deployProxy(SpinMachineV1);
+    //let Brlc = await ethers.getContractFactory('brlc_token');
+    //let brlc = await upgrades.deployProxy(brlc_token);
     await spinMachine.deployed();
     [owner, addr1, addr2] = await ethers.getSigners();
+    //brlc_token = await spinMachine.token()
+    console.log(spinMachine.address)
   });
 
   describe("Gamification tests", function() {
@@ -52,16 +65,15 @@ describe("smart-rewards tests", async function() {
     });
 
     it("Should buy extra spin", async function() {
-      await spinMachine.buyExtraSpin(addr2.address, 1)
+      await spinMachine.connect(signer).buyExtraSpin(addr2.address, 1)
+      //console.log(brlc_token)
       
       //Error: VM Exception while processing transaction: revert Address: call to non-contract
     });
 
     it("Should fail if owner or addr1 can't spin", async function() {
-      let addr1CanSpin: boolean;
-      let ownerCanSpin: boolean;
-      addr1CanSpin = await spinMachine.canSpinFor(addr1.address);
-      ownerCanSpin = await spinMachine.canSpin();
+      let addr1CanSpin: boolean = await spinMachine.canSpinFor(addr1.address);
+      let ownerCanSpin: boolean = await spinMachine.canSpin();
       expect(addr1CanSpin).to.equal(true)
       expect(ownerCanSpin).to.equal(true)
     });
@@ -70,7 +82,7 @@ describe("smart-rewards tests", async function() {
       //let success: boolean;
       //let winnings: number;
       let result: any;
-      result = await spinMachine.connect(owner).spin(); //Error: Transaction reverted: function call to a non-contract account
+      //result = await spinMachine.connect(brlc_token).spin(); //Error: Transaction reverted: function call to a non-contract account
       //console.log(result);
     });
   });
