16e8a494be445ca7dc649c8617f2557883e69a34
moreira-a <arthur.moreira@cloudwalk.io>
2021-05-04 15:01:26 -0300

    update config and packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
-import { task } from "hardhat/config";
+import { task, HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
+import '@typechain/hardhat';
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,7 +20,21 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.6.12",
-};
+//module.exports = {
+//  solidity: "0.6.12",
+//};
 
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: '0.6.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  },
+}
+
+export default config
