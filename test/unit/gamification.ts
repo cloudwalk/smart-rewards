e2e36b2f7630014cbf8fd0d2d61fa9aee3b15bcc
moreira-a <arthur.moreira@cloudwalk.io>
2021-06-28 00:09:17 -0300

    improve tests, add more validations

--- test/unit/gamification.ts
+++ test/unit/gamification.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades, waffle } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { ContractFactory, Contract } from "ethers";
@@ -11,6 +11,7 @@ describe("gamification tests", async function() {
   let bob: SignerWithAddress;
   let BRLCFactory: ContractFactory;
   let BRLC: Contract;
+  let SpinMachineInterface
   
   beforeEach(async function() {
     BRLCFactory = await ethers.getContractFactory("ERC20Mock");
@@ -21,24 +22,34 @@ describe("gamification tests", async function() {
     SpinMachineV1 = await ethers.getContractFactory("SpinMachineV1");
     spinMachine = await upgrades.deployProxy(SpinMachineV1, [BRLC.address]);
     await spinMachine.deployed();
+    SpinMachineInterface = spinMachine.interface
   });
 
   describe("SpinMachineV1 tests", function() {
 
-    it("Should set the right owner and token", async function () {
+    it("Should set the right owner and token", async function() {
       expect(await spinMachine.owner()).to.equal(owner.address);
       expect(await spinMachine.token()).to.equal(BRLC.address);
     });
 
     it("Should set prizes correctly", async function() {
-      let prizes = [1,2,3];
-      let distribution_event = await spinMachine.setPrizes(prizes);
-      let new_prizes = await spinMachine.getPrizes()
+      const prizes: number[] = [1,2,3];
+      const distribution_event = await spinMachine.setPrizes(prizes);
+      const new_prizes = await spinMachine.getPrizes()
       expect(new_prizes[0]).to.equal(1);
       expect(new_prizes[1]).to.equal(2);
       expect(new_prizes[2]).to.equal(3);
       expect(distribution_event).to.emit(spinMachine, "PrizesDistributionChanged").withArgs(new_prizes);
     });
+
+    it("Should set spin delays correctly", async function() {
+      const old_spin_delay = await spinMachine.freeSpinDelay();
+      const new_spin_delay: number = 2;
+      const spin_delay_event = await spinMachine.setFreeSpinDelay(new_spin_delay);
+      expect(await spinMachine.freeSpinDelay()).to.equal(2);
+      expect(spin_delay_event).to.emit(spinMachine, "FreeSpinDelayChanged").withArgs(new_spin_delay, old_spin_delay);
+    });
+
 
     it("Should set a new free spin delay", async function() {
       let previous_free_spin_delay: number;
@@ -50,26 +61,93 @@ describe("gamification tests", async function() {
     });
 
     it("Should set a new extra spin price", async function() {
-      let new_free_spin_price: number;
-      new_free_spin_price = 100;
-      await spinMachine.setExtraSpinPrice(new_free_spin_price);
+      const old_price = await spinMachine.extraSpinPrice();
+      const new_price: number = 100;
+      const result = await spinMachine.setExtraSpinPrice(new_price);
       expect(await spinMachine.extraSpinPrice()).to.equal(100);
+      expect(result).to.emit(spinMachine, "ExtraSpinPriceChanged").withArgs("100", old_price);
     });
 
     it("Should buy extra spin", async function() {
-      let result = await spinMachine.buyExtraSpin(alice.address, 1);
+      //await spinMachine.setExtraSpinPrice(ethers.utils.parseEther("0.000000000000000001"));
+      //const extra_spin_price = await spinMachine.extraSpinPrice();
+      //console.log(extra_spin_price.toString());
+      let owner_balance = await owner.getBalance()
+      //console.log(ethers.utils.formatEther(owner_balance));
+      const result = await spinMachine.connect(owner).buyExtraSpin(alice.address, 1);
+      const alice_extra_spins = await spinMachine.extraSpins(alice.address);
+      owner_balance = await owner.getBalance()
+      //check if correct token amount was transfered from owner to spinMachine
       expect(result).to.emit(spinMachine, "ExtraSpinPurchased").withArgs(owner.address, alice.address, "1");
+      expect(alice_extra_spins.toString()).to.equal("1");
+    });
+
+    it("Should grant extra spin", async function() {
+      const result = await spinMachine.grantExtraSpin(bob.address, 1);
+      expect(result).to.emit(spinMachine, "ExtraSpinGranted").withArgs(owner.address, bob.address, "1");
     });
 
     it("Should fail if owner or alice can't spin", async function() {
-      let aliceCanSpin: boolean = await spinMachine.canSpinFor(alice.address);
-      let ownerCanSpin: boolean = await spinMachine.canSpin();
-      expect(aliceCanSpin).to.equal(true)
-      expect(ownerCanSpin).to.equal(true)
+      let alice_can_spin: boolean = await spinMachine.canSpinFor(alice.address);
+      let owner_can_spin: boolean = await spinMachine.canSpin();
+      expect(alice_can_spin).to.equal(true)
+      expect(owner_can_spin).to.equal(true)
     });
 
-    it("Should sucessfully spin", async function() {
-      expect(await spinMachine.spin()).to.emit(spinMachine, "Spin").withArgs(owner.address, "0", "0", false);
+    it("Should sucessfully spin n times if delay is zero", async function() {
+      await spinMachine.setFreeSpinDelay(0);
+      const number_of_spins: number = 5;
+      for(let i = 0; i < number_of_spins; i++) {
+        let res = await spinMachine.connect(bob).spin();
+        res = await res.wait()
+        let args = SpinMachineInterface.parseLog(res.logs[0]).args
+
+        let winnings = args.winnings.toString();
+        let sent = args.sent.toString();
+        let extra = args.extra;
+        expect(extra).to.equal(false);
+        expect(await spinMachine.connect(bob).spin()).to.emit(spinMachine, "Spin").withArgs(bob.address, winnings, sent, extra);
+      }
+    });
+    
+    it("Should spin twice if there is delay but has one extra spin", async function() {
+      await spinMachine.grantExtraSpin(bob.address, 1);
+      let res = await spinMachine.connect(bob).spin();
+      res = await res.wait()
+      let args = SpinMachineInterface.parseLog(res.logs[0]).args
+
+      let winnings = args.winnings.toString();
+      let sent = args.sent.toString();
+      let extra = args.extra;
+      expect(extra).to.equal(false);
+      
+      // extra spin
+      res = await spinMachine.connect(bob).spin()
+      res = await res.wait()
+      args = SpinMachineInterface.parseLog(res.logs[0]).args
+
+      winnings = args.winnings.toString();
+      sent = args.sent.toString();
+      extra = args.extra;
+      expect(extra).to.equal(true);
+
+      // find a way to check if spin() really happened, can't access 'success'
+      // maybe use Waffle's method to check if function _winnings() was called
+    });
+
+    it("Should not spin more than once if there is delay and no extras", async function() {
+      let res = await spinMachine.spin.call({from: bob.address});
+      console.log(res)
+      res = await res.wait()
+      let logs = SpinMachineInterface.parseLog(res.logs[0])
+      let args = logs.args
+
+      let winnings = args.winnings.toString();
+      let sent = args.sent.toString();
+      let extra = args.extra;
+      expect(extra).to.equal(false);
+      // find a way to check if spin() really happened, can't access 'success'
+      // maybe use Waffle's method to check if function _winnings() was called
     });
   });
 });
